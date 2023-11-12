4dafc5eab3366082e9f0e6e3efa443dcdec6859a
William Garcia <wwoodsgarcia@ufl.edu>
2023-11-12 00:46:57 -0500

    added new page for  Property Analaysis. Also installed npm dependencies we need to display the charts

---
+++ JavaScript/Javascript/propertyAnal.js
@@ -0,0 +1,23 @@
+"strict";
+const ctx = document.getElementById("deviation-chart").getContext("2d");
+
+const chart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
