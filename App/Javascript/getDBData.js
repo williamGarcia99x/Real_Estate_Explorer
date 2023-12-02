3a6423440ae681211aede5876b3f535c3cc05436
RaySoftware <16505434+RaySoftware@users.noreply.github.com>
2023-12-02 10:13:02 +0000

    Complete page 1 connections and queries.

--- App/Javascript/getDBData.js
+++ App/Javascript/getDBData.js
@@ -29,100 +29,123 @@ function getDBData(socketQuery){
     landSize = document.getElementById("land-size").value
   }
   // Page 2: Affordability
+  else if (socketQuery == "getAffordability")
+  {
+    county = document.getElementById("county-land").value
+    startDate = document.getElementById("start-date-land").value
+    endDate = document.getElementById("end-date-land").value
+    landSize = document.getElementById("land-size").value
+  }
   // Page 3: Neighborhood
 
-  let jsonRequest = {
-    "requestType": socketQuery,
-    "options": {
-      "county": county,
-      "startDate": startDate,
-      "endDate": endDate,
-      "landSize": landSize,
-      "propertyAge": propertyAge
-    }
+  if (socketQuery == "getDeviation" && (startDate.length == 0 || endDate.length == 0) && county != "default")
+  {
+    alert("Please add all input.")
   }
-    // Connect to the application server
-    const socket = new WebSocket("ws://10.228.10.193:1337/");
+  else if (socketQuery == "getAgeInfluence" && (startDate.length == 0 || endDate.length == 0 || propertyAge.length == 0) && county != "default")
+  {
+    alert("Please add all input.")
+  }
+  else if (socketQuery == "getSaleLandsize" && (startDate.length == 0 || endDate.length == 0 || landSize.length == 0) && county != "default")
+  {
+    alert("Please add all input.")
+  }
+  else {
+    let jsonRequest = {
+      "requestType": socketQuery,
+      "options": {
+        "county": county,
+        "startDate": startDate,
+        "endDate": endDate,
+        "landSize": landSize,
+        "propertyAge": propertyAge,
+        "houseHoldSize1" : "",
+        "houseHoldSize2" : ""
+      }
+    }
+      // Connect to the application server
+      const socket = new WebSocket("ws://10.228.10.193:1337/");
 
-    // Turn the previous JSON text to a string and send it to the server
-    socket.addEventListener("open", (event) => {
-        socket.send(JSON.stringify(jsonRequest));
-    });
+      // Turn the previous JSON text to a string and send it to the server
+      socket.addEventListener("open", (event) => {
+          socket.send(JSON.stringify(jsonRequest));
+      });
 
-    // Receive data from the server
-    socket.addEventListener("message", (event) => {
-        console.log(event.data)
-        if (socketQuery == "getAllTuples")
-        {
-            document.getElementById("record-count").textContent = event.data + " records";
-        }
-        // For all other requests: Receive the server's JSON containing dates and values arrays
-        else if (socketQuery == "getDeviation")
-        {
-            // Destroy the previously created chart to add the new one
-            if (Chart.getChart("deviation-chart") != null)
-            {
-              Chart.getChart("deviation-chart").destroy();
-            }
-            
-            console.log(event.data)
-            const json = JSON.parse(event.data);
-            const deviationChartData = {
-                labels: json.dates,
-                datasets: [
-                  {
-                    label: "Deviation from Appraisal Value",
-                    data: json.values,
-                    borderWidth: 1,
-                    borderColor: "rgba(255, 99, 132, 1)",
-                  },
-                ],
-              };
-            const deviationChart = renderChart("deviation-chart", deviationChartData);
-        }
-        else if (socketQuery == "getAgeInfluence")
-        { 
-            if (Chart.getChart("age-chart") != null)
-            {
-              Chart.getChart("age-chart").destroy();
-            }
+      // Receive data from the server
+      socket.addEventListener("message", (event) => {
+          console.log(event.data)
+          if (socketQuery == "getAllTuples")
+          {
+              document.getElementById("record-count").textContent = event.data + " records";
+          }
+          // For all other requests: Receive the server's JSON containing dates and values arrays
+          else if (socketQuery == "getDeviation")
+          {
+              // Destroy the previously created chart to add the new one
+              if (Chart.getChart("deviation-chart") != null)
+              {
+                Chart.getChart("deviation-chart").destroy();
+              }
+              
+              console.log(event.data)
+              const json = JSON.parse(event.data);
+              const deviationChartData = {
+                  labels: json.dates,
+                  datasets: [
+                    {
+                      label: "Deviation from Appraisal Value (%)",
+                      data: json.values,
+                      borderWidth: 1,
+                      borderColor: "rgba(255, 99, 132, 1)",
+                    },
+                  ],
+                };
+              const deviationChart = renderChart("deviation-chart", deviationChartData);
+          }
+          else if (socketQuery == "getAgeInfluence")
+          { 
+              if (Chart.getChart("age-chart") != null)
+              {
+                Chart.getChart("age-chart").destroy();
+              }
 
-            console.log(event.data)
-            const json = JSON.parse(event.data);
-            const deviationChartData = {
-                labels: json.dates,
-                datasets: [
-                  {
-                    label: "Influence of Age on Market Value",
-                    data: json.values,
-                    borderWidth: 1,
-                    borderColor: "rgba(255, 99, 132, 1)",
-                  },
-                ],
-              };
-            const deviationChart = renderChart("age-chart", deviationChartData);
-        }
-        else if (socketQuery == "getSaleLandsize")
-        {
-            if (Chart.getChart("land-size-chart") != null)
-            {
-              Chart.getChart("land-size-chart").destroy();
-            }
+              console.log(event.data)
+              const json = JSON.parse(event.data);
+              const deviationChartData = {
+                  labels: json.dates,
+                  datasets: [
+                    {
+                      label: "Influence of Age on Market Value",
+                      data: json.values,
+                      borderWidth: 1,
+                      borderColor: "rgba(255, 99, 132, 1)",
+                    },
+                  ],
+                };
+              const deviationChart = renderChart("age-chart", deviationChartData);
+          }
+          else if (socketQuery == "getSaleLandsize")
+          {
+              if (Chart.getChart("land-size-chart") != null)
+              {
+                Chart.getChart("land-size-chart").destroy();
+              }
 
-            console.log(event.data)
-            const json = JSON.parse(event.data);
-            const deviationChartData = {
-                labels: json.dates,
-                datasets: [
-                  {
-                    label: "Sales for Properties of Various Land Sizes",
-                    data: json.values,
-                    borderWidth: 1,
-                    borderColor: "rgba(255, 99, 132, 1)",
-                  },
-                ],
-              };
-            const deviationChart = renderChart("land-size-chart", deviationChartData);
-        }
-    });
+              console.log(event.data)
+              const json = JSON.parse(event.data);
+              const deviationChartData = {
+                  labels: json.dates,
+                  datasets: [
+                    {
+                      label: "Sales for Properties of Various Land Sizes",
+                      data: json.values,
+                      borderWidth: 1,
+                      borderColor: "rgba(255, 99, 132, 1)",
+                    },
+                  ],
+                };
+              const deviationChart = renderChart("land-size-chart", deviationChartData);
+          }
+      });
+  }
 }
--- Server/app_server.py
+++ Server/app_server.py
@@ -18,38 +18,164 @@ async def exchangeQueryData(cursor, websocket):
                         (SELECT COUNT(*) FROM MORTGAGERATES) FROM dual")
             # Get the first row, which is just the single sum
             to_send = str(cursor.fetchone()[0])
-            # Temporary example for charts
         case "getDeviation":
-            cursor.execute("SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE FROM (SELECT SALE_MO, SALE_YR, COUNT(PARCEL_ID) as VALUE FROM PROPERTY WHERE SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September'OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December' GROUP BY SALE_MO, SALE_YR) ORDER BY SALE_DATE ASC)")
+            if data["options"]["county"] == "default":
+                cursor.execute("SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT SALE_MO, SALE_YR, AVG(((SALE_PRC / MARKET_VALUE) - 1) * 100) as VALUE \
+                        FROM PROPERTY \
+                        WHERE MARKET_VALUE != 0 AND (SALE_PRC / MARKET_VALUE) - 1 >= 0 \
+                        AND(SALE_PRC / MARKET_VALUE) - 1 <= 10 \
+                        AND (SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' \
+                        OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' \
+                        OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September'\
+                        OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December') \
+                        GROUP BY SALE_MO, SALE_YR) \
+                        ORDER BY SALE_DATE ASC)")
+            else:
+                cursor.execute(f"SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT SALE_MO, SALE_YR, AVG(((SALE_PRC / MARKET_VALUE) - 1) * 100) as VALUE \
+                        FROM PROPERTY \
+                        WHERE MARKET_VALUE != 0 AND (SALE_PRC / MARKET_VALUE) - 1 >= 0 \
+                        AND COUNTY_CODE = {data['options']['county']} AND (SALE_PRC / MARKET_VALUE) - 1 <= 10 \
+                        AND (SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' \
+                        OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' \
+                        OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September'\
+                        OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December') \
+                        GROUP BY SALE_MO, SALE_YR) \
+                        ORDER BY SALE_DATE ASC)\WHERE SALE_DATE BETWEEN TO_DATE('{data['options']['startDate']}', 'YYYY-MM') AND TO_DATE('{data['options']['endDate']}', 'YYYY-MM')")
+                        
+
             chart_data = {
                     "dates" : [],
                     "values" : []
             }
+
+            # For each row, append the date and the count separatly to the dictionary
+            for row in cursor.fetchall():
+                chart_data["dates"].append(str(row[0]))
+                chart_data["values"].append(str(row[1]))
+
             # Convert to JSON and then to a string
             to_send = str(json.dumps(chart_data))
         case "getAgeInfluence":
-            cursor.execute("SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE FROM (SELECT SALE_MO, SALE_YR, COUNT(PARCEL_ID) as VALUE FROM PROPERTY WHERE SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September'OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December' GROUP BY SALE_MO, SALE_YR) ORDER BY SALE_DATE ASC)")
+            min_year = 0
+            max_year = 0
+            if data["options"]["propertyAge"] == "0-9":
+                min_year = 0
+                max_year = 9
+            elif data["options"]["propertyAge"] == "10-19":
+                min_year = 10
+                max_year = 19
+            elif data["options"]["propertyAge"] == "20-29":
+                min_year = 20
+                max_year = 29
+            elif data["options"]["propertyAge"] == "30-39":
+                min_year = 30
+                max_year = 39
+            elif data["options"]["propertyAge"] == "40":
+                min_year = 40
+                max_year = 999
+            if data["options"]["county"] == "default":
+                cursor.execute(f"SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT SALE_MO, SALE_YR, COUNT(PARCEL_ID) as VALUE \
+                        FROM PROPERTY \
+                        WHERE (2023 - BUILT_YEAR) >= 0 AND (2023 - BUILT_YEAR) <= 1000 \
+                        AND (SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' \
+                        OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' \
+                        OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September' \
+                        OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December') \
+                        GROUP BY SALE_MO, SALE_YR) \
+                        ORDER BY SALE_DATE ASC) \
+                        ")
+            else:
+                cursor.execute(f"SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT SALE_MO, SALE_YR, COUNT(PARCEL_ID) as VALUE \
+                        FROM PROPERTY \
+                        WHERE (2023 - BUILT_YEAR) >= {min_year} AND (2023 - BUILT_YEAR) <= {max_year} \
+                        AND COUNTY_CODE = {data['options']['county']} \
+                        AND (SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' \
+                        OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' \
+                        OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September' \
+                        OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December') \
+                        GROUP BY SALE_MO, SALE_YR) \
+                        ORDER BY SALE_DATE ASC) \
+                        WHERE SALE_DATE BETWEEN TO_DATE('{data['options']['startDate']}', 'YYYY-MM') AND TO_DATE('{data['options']['endDate']}', 'YYYY-MM')")
             chart_data = {
                     "dates" : [],
                     "values" : []
             }
+            
+            # For each row, append the date and the count separatly to the dictionary
+            for row in cursor.fetchall():
+                chart_data["dates"].append(str(row[0]))
+                chart_data["values"].append(str(row[1]))
+                
             # Convert to JSON and then to a string
             to_send = str(json.dumps(chart_data))
         case "getSaleLandsize":
-            cursor.execute("SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE FROM (SELECT SALE_MO, SALE_YR, COUNT(PARCEL_ID) as VALUE FROM PROPERTY WHERE SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September'OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December' GROUP BY SALE_MO, SALE_YR) ORDER BY SALE_DATE ASC)")
+            min_size = 0
+            max_size = 0
+            if data["options"]["landSize"] == "0-999":
+                min_size = 0
+                max_size= 999
+            elif data["options"]["landSize"] == "1000-4999":
+                min_size = 1000
+                max_size = 4999
+            elif data["options"]["landSize"] == "5000-9999":
+                min_size = 29999
+                max_size = 9999
+            elif data["options"]["landSize"] == "20000-29999":
+                min_size = 20000
+                max_size = 29999
+            elif data["options"]["landSize"] == "30000":
+                min_size = 30000
+                max_size = 99999999
+
+            if data["options"]["county"] == "default":
+                cursor.execute(f"SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT SALE_MO, SALE_YR, COUNT(PARCEL_ID) as VALUE \
+                        FROM PROPERTY \
+                        WHERE LANDSIZE >= 0 AND LANDSIZE <= 99999999 \
+                        AND (SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' \
+                        OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' \
+                        OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September' \
+                        OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December') \
+                        GROUP BY SALE_MO, SALE_YR) \
+                        ORDER BY SALE_DATE ASC) \
+                        ")
+            else:
+                cursor.execute(f"SELECT TO_CHAR(SALE_DATE, 'Mon YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT TO_DATE(SALE_MO || ' ' || SALE_YR, 'MON YYYY') as SALE_DATE, VALUE \
+                        FROM (SELECT SALE_MO, SALE_YR, COUNT(PARCEL_ID) as VALUE \
+                        FROM PROPERTY \
+                        WHERE LANDSIZE >= {min_size} AND LANDSIZE <= {max_size} \
+                        AND COUNTY_CODE = {data['options']['county']} \
+                        AND (SALE_MO = 'January' OR SALE_MO = 'February' OR SALE_MO = 'March' \
+                        OR SALE_MO = 'April' OR SALE_MO = 'May' OR SALE_MO = 'June' \
+                        OR SALE_MO = 'July' OR SALE_MO = 'August' OR SALE_MO = 'September' \
+                        OR SALE_MO = 'October' OR SALE_MO = 'November' OR SALE_MO = 'December') \
+                        GROUP BY SALE_MO, SALE_YR) \
+                        ORDER BY SALE_DATE ASC) \
+                        WHERE SALE_DATE BETWEEN TO_DATE('{data['options']['startDate']}', 'YYYY-MM') AND TO_DATE('{data['options']['endDate']}', 'YYYY-MM')")
             chart_data = {
                     "dates" : [],
                     "values" : []
             }
+
+            # For each row, append the date and the count separately to the dictionary
+            for row in cursor.fetchall():
+                chart_data["dates"].append(str(row[0]))
+                chart_data["values"].append(str(row[1]))
+
             # Convert to JSON and then to a string
             to_send = str(json.dumps(chart_data))
         case _:
             to_send = "null"
-
-    # For each row, append the date and the count separatly to the dictionary
-    for row in cursor.fetchall():
-        chart_data["dates"].append(str(row[0]))
-        chart_data["values"].append(str(row[1]))
 
     await websocket.send(str(to_send))
 
